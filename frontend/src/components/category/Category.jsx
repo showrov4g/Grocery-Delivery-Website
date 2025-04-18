4f035e5bc9031fccbffb0252c54dcf97c5287770
showrov4g <showrovg00@gmail.com>
2025-04-19 00:30:11 +0600

    category page design

--- frontend/src/components/category/Category.jsx
+++ frontend/src/components/category/Category.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Category = () => {
   return (
-    <div>
-      
+    <div className='mt-16'>
+      <p className='text-2xl md:text-3xl font-medium'>Categories</p>
+      <div>
+        <div>
+            <img src={assets.box_icon} alt="" />
+            <p>Fruit</p>
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import MainBanner from '../../components/mainBanner/MainBanner'
+import Category from '../../components/category/Category'
 
 const Home = () => {
   return (
     <div className='mt-10'>
       <MainBanner />
+      <Category/>
     </div>
   )
 }
