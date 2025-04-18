e83b0fe97c38c92eb9c368ef0f3cb2e032d88ebb
showrov4g <showrovg00@gmail.com>
2025-04-18 18:56:10 +0600

    navbar design complete

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         <div>
             <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
-                <NavLink to='/'>
+                <NavLink to='/' onClick={()=>setOpen(false)}>
                     <img className="h-9" src={assets.logo} alt="dummyLogoColored" />
                 </NavLink>
 
@@ -27,23 +27,32 @@ const Navbar = () => {
                     <NavLink to="/">Home</NavLink>
                     <NavLink to="/products">All Products</NavLink>
                     <NavLink to={'/contact'}>Contact</NavLink>
-
+                    {/* search  */}
                     <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
                         <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
                         <img src={assets.search_icon} alt="search" className='w-4 h-4' />
                     </div>
-
-                    <div className="relative cursor-pointer">
-                        <img src={assets.cart_icon} alt="Cart icon" className='w-4 h-4 opacity-80' />
+                    {/* Cart  */}
+                    <div onClick={()=>navigate('/cart')} className="relative cursor-pointer">
+                        <img src={assets.nav_cart_icon} alt="Cart icon" className='w-6 opacity-80' />
                         <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
                     </div>
 
-                   {!user ? (<button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+                    {/* login button  */}
+                   {!user ? (<button onClick={()=>setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
                         Login
                     </button>): (
-                        <div></div>
+                        <div className='relative group'>
+                            <img src={assets.profile_icon} alt="profile icon" className='w-10' />
+                            <ul className='hidden group-hover:block  absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-30 rounded-md text-sm z-40'>
+                                <li onClick={()=>navigate("/my-orders")} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
+                                <li onClick={logout} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
+                            </ul>
+                        </div>
                     )}
                 </div>
+
+                {/* mobile menu  */}
 
                 <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
                     {/* Menu Icon SVG */}
@@ -56,7 +65,7 @@ const Navbar = () => {
                     <NavLink to="/" onClick={() => setOpen(false)}>Home</NavLink>
                     <NavLink to="/products" onClick={() => setOpen(false)}>All Products</NavLink>
                     {
-                        user && <NavLink to="/myorders" onClick={() => setOpen(false)}>My orders</NavLink>
+                        user && <NavLink to="/my-orders" onClick={() => setOpen(false)}>My orders</NavLink>
                     }
                     <NavLink to="/contact" onClick={() => setOpen(false)}>Contact</NavLink>
                     {!user ? (<button onClick={() => { 
