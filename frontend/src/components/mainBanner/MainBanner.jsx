a3072e0ad2487469a55f5876f8f27a5e4c927b18
showrov4g <showrovg00@gmail.com>
2025-04-18 22:35:50 +0600

    font style

--- frontend/src/components/mainBanner/MainBanner.jsx
+++ frontend/src/components/mainBanner/MainBanner.jsx
@@ -9,10 +9,10 @@ const MainBanner = () => {
             <img src={assets.main_banner_bg_sm} alt="banner background" className='w-full md:hidden' />
             {/* add text  */}
             <div>
-                <h1>Freshness You Can Trust, Savings You will Love!</h1>
+                <h1 className=''>Freshness You Can Trust, Savings You will Love!</h1>
             </div>
             {/* buttons  */}
-            <div>
+            <div className='flex items-center mt-6 font-medium'>
                 <Link to="/products" className='group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white cursor-pointer'>Shop Now <img className='md:hidden transition group-focus:translate-x-1' src={assets.white_arrow_icon} alt="" /></Link>
                 {/* second link  */}
                 <Link className='group flex items-center gap-2 px-7 md:px-9 py-3 cursor-pointer'>Explore deals <img className='transition group-focus:translate-x-1' src={assets.black_arrow_icon} alt="" /></Link>
