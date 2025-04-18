a3406c51bfdc065b6cda2d95b2d151cba9dd0b25
showrov4g <showrovg00@gmail.com>
2025-04-18 21:04:24 +0600

    Banner page design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import MainBanner from './components/mainBanner/MainBanner'
-import { Route, Routes } from 'react-router'
+import { Route, Routes, useLocation } from 'react-router'
 import Home from './pages/home/Home'
 
 const App = () => {
+  const isSellerPath = useLocation().pathname.includes("seller")
+
   return (
     <div>
-      <Navbar/>
-      <div className='px-6 md:px-16 lg:px-24 xl:px-32'>
+      {isSellerPath? "": <Navbar />}
+      <div className={`${isSellerPath ? "" : " px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
-          <Route path='/' element={<Home/>}></Route>
+          <Route path='/' element={<Home />}></Route>
         </Routes>
       </div>
     </div>
