aa79892313c40eacf797229e296416948f78e3dc
showrov4g <showrovg00@gmail.com>
2025-04-18 20:40:01 +0600

    home page setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import MainBanner from './components/mainBanner/MainBanner'
 import { Route, Routes } from 'react-router'
+import Home from './pages/home/Home'
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Navbar/>
       <div>
         <Routes>
-          <Route></Route>
+          <Route path='/' element={<Home/>}></Route>
         </Routes>
       </div>
     </div>
