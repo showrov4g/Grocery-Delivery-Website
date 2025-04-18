1639d2e4e6a50c4818951fb1cd0b45015e62a9fa
showrov4g <showrovg00@gmail.com>
2025-04-18 22:13:10 +0600

    button design

--- frontend/src/components/mainBanner/MainBanner.jsx
+++ frontend/src/components/mainBanner/MainBanner.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 
 const MainBanner = () => {
-  return (
-    <div className='relative'>
-      <img src={assets.main_banner_bg} alt="banner background" className='w-full hidden md:block' />
-      <img src={assets.main_banner_bg_sm} alt="banner background" className='w-full md:hidden' />
-    </div>
-  )
+    return (
+        <div className='relative'>
+            <img src={assets.main_banner_bg} alt="banner background" className='w-full hidden md:block' />
+            <img src={assets.main_banner_bg_sm} alt="banner background" className='w-full md:hidden' />
+            {/* add text  */}
+            <div>
+                <h1>Freshness You Can Trust, Savings You will Love!</h1>
+            </div>
+            {/* buttons  */}
+            <div>
+                <Link to="/products" className='group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white cursor-pointer'>Shop Now <img className='md:hidden transition group-focus:translate-x-1' src={assets.white_arrow_icon} alt="" /></Link>
+                {/* second link  */}
+                <Link className='group flex items-center gap-2 px-7 md:px-9 py-3 cursor-pointer'>Explore deals <img className='transition group-focus:translate-x-1' src={assets.black_arrow_icon} alt="" /></Link>
+            </div>
+        </div>
+    )
 }
 
-export default MainBanner
+export default MainBanner;
