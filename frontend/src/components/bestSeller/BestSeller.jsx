41ca04515c6f961c54a0ab5f30ed863f04084542
showrov4g <showrovg00@gmail.com>
2025-04-19 09:47:45 +0600

    product card design

--- frontend/src/components/bestSeller/BestSeller.jsx
+++ frontend/src/components/bestSeller/BestSeller.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const BestSeller = () => {
   return (
-    <div>
-      
+    <div className='mt-16'>
+      <p className='text-2xl md:text-3xl font-medium'>Best Sellers </p>
+      {/* product list */}
+      <div>
+
+      </div>
     </div>
   )
 }
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import MainBanner from '../../components/mainBanner/MainBanner'
 import Category from '../../components/category/Category'
+import BestSeller from '../../components/bestSeller/BestSeller'
 
 const Home = () => {
   return (
     <div className='mt-10'>
       <MainBanner />
       <Category/>
+      <BestSeller/>
     </div>
   )
 }
