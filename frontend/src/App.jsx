f45693cf2e692a140f79f6905d673803b45b90cb
showrov4g <showrovg00@gmail.com>
2025-04-19 11:21:34 +0600

    product cart page design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import Navbar from './components/navbar/Navbar'
 import MainBanner from './components/mainBanner/MainBanner'
 import { Route, Routes, useLocation } from 'react-router'
 import Home from './pages/home/Home'
+import  { Toaster } from 'react-hot-toast';
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller")
 
   return (
     <div>
+      <Toaster />
       {isSellerPath? "": <Navbar />}
       <div className={`${isSellerPath ? "" : " px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,22 +1,40 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
 
 export const AppContextProvider = ({ children }) => {
+    // currency data 
+    const currency = import.meta.VITE_CURRENCY
+
     const navigate = useNavigate();
     // state variables 
     const [user, setUser] = useState(null);
     const [isSeller, setIsSeller] = useState(false);
     const [showUserLogin, setShowUserLogin] = useState(false);
     const [products,setProducts] = useState([]);
+    const [cartItems,setCartItems] = useState({})
+
 
     // product data fetch 
     const fetchProduct = async()=>{
         setProducts(dummyProducts);
     }
+    // add product to cart 
+    const addToCart =()=>{
+        let cartData = structuredClone(cartItems);
+        if(cartData[itemId]){
+            cartData[itemId] += 1;
+        }else{
+            cartData[itemId] = 1;
+        }
+        setCartItems(cartData);
+        toast.success("Added to cart");
+    }
+
     useEffect(()=>{
         fetchProduct();
     },[])
@@ -30,7 +48,9 @@ export const AppContextProvider = ({ children }) => {
         isSeller,
         setIsSeller,
         showUserLogin,
-        setShowUserLogin
+        setShowUserLogin,
+        products,
+        currency
     }
     return <AppContext.Provider value={value}>
         {children}
