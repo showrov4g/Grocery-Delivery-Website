6990d5b5511a4006fad955f491dcd8edf67668c2
showrov4g <showrovg00@gmail.com>
2025-04-20 19:34:29 +0600

    features text add

--- frontend/src/components/bottomBanner/BottomBanner.jsx
+++ frontend/src/components/bottomBanner/BottomBanner.jsx
@@ -6,6 +6,10 @@ const BottomBanner = () => {
     <div className='relative mt-24'>
         <img src={assets.bottom_banner_image} alt="bottom_banner_image" className='w-full hidden md:block' />
         <img src={assets.bottom_banner_image_sm} alt="bottom_banner_image" className='w-full md:hidden' />
+        {/* features text add  */}
+        <div className='absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24'>
+
+        </div>
     </div>
   )
 }
