c2054440c2309ccb16339af505cd422a17441994
showrov4g <showrovg00@gmail.com>
2025-04-21 09:52:07 +0600

    Login and Logout page design

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
     return (
-        <div>
+        <div className='fixed top-0 bottom-0 right-0 left-0 z-30 flex items-center text-sm text-gray-600 bg-black/50'>
             <form className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] rounded-lg shadow-xl border border-gray-200 bg-white">
                 <p className="text-2xl font-medium m-auto">
                     <span className="text-indigo-500">User</span> {state === "login" ? "Login" : "Sign Up"}
