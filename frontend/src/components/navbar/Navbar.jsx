2df19bd302e87351284f6b6ef6bad4f00fcccc16
showrov4g <showrovg00@gmail.com>
2025-04-22 00:40:28 +0600

    search function build

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -16,8 +16,12 @@ const Navbar = () => {
     }
     // useEffect 
     useEffect(()=>{
-        
-    },[])
+        if(searchQuery.length>0){
+            navigate('/all-product')
+        }else{
+
+        }
+    },[searchQuery])
 
     return (
         <div>
@@ -34,7 +38,7 @@ const Navbar = () => {
                     <NavLink to={'/contact'}>Contact</NavLink>
                     {/* search  */}
                     <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-                        <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
+                        <input onChange={(e)=>setSearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
                         <img src={assets.search_icon} alt="search" className='w-4 h-4' />
                     </div>
                     {/* Cart  */}
