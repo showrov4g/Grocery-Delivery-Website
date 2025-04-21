b163c0252e1a44d05544502590fd235f6a048608
showrov4g <showrovg00@gmail.com>
2025-04-21 11:51:24 +0600

    update text color

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -7,12 +7,12 @@ const Login = () => {
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
     const {setShowUserLogin} = useAppContext();
-    
+
     return (
         <div onClick={()=>setShowUserLogin(false)} className='fixed top-0 bottom-0 right-0 left-0 z-30 flex items-center text-sm text-gray-600 bg-black/50'>
             <form onClick={(e)=>e.stopPropagation()} className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] rounded-lg shadow-xl border border-gray-200 bg-white">
                 <p className="text-2xl font-medium m-auto">
-                    <span className="text-indigo-500">User</span> {state === "login" ? "Login" : "Sign Up"}
+                    <span className="text-primary">User</span> {state === "login" ? "Login" : "Sign Up"}
                 </p>
                 {state === "register" && (
                     <div className="w-full">
@@ -30,14 +30,14 @@ const Login = () => {
                 </div>
                 {state === "register" ? (
                     <p>
-                        Already have account? <span onClick={() => setState("login")} className="text-indigo-500 cursor-pointer">click here</span>
+                        Already have account? <span onClick={() => setState("login")} className="text-primary cursor-pointer">click here</span>
                     </p>
                 ) : (
                     <p>
-                        Create an account? <span onClick={() => setState("register")} className="text-indigo-500 cursor-pointer">click here</span>
+                        Create an account? <span onClick={() => setState("register")} className="text-primary cursor-pointer">click here</span>
                     </p>
                 )}
-                <button className="bg-indigo-500 hover:bg-indigo-600 transition-all text-white w-full py-2 rounded-md cursor-pointer">
+                <button className="bg-primary hover:bg-primary-dull transition-all text-white w-full py-2 rounded-md cursor-pointer">
                     {state === "register" ? "Create Account" : "Login"}
                 </button>
             </form>
