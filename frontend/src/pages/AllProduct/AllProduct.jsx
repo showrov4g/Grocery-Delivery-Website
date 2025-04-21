387995eb0b052ca0b8d82118057449ac2f6d1435
showrov4g <showrovg00@gmail.com>
2025-04-21 23:22:57 +0600

    all product page design

--- frontend/src/pages/AllProduct/AllProduct.jsx
+++ frontend/src/pages/AllProduct/AllProduct.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useActionState } from 'react'
 
 const AllProduct = () => {
-    const { products } = useActionState()
+    const { products } = useActionState();
+    // state variable
+    const [filterProduct, SetFilterProduct] = useState();
     return (
         <div className='mt-16 flex flex-col'>
             <div className='flex flex-col items-end w-max'>
                 <p className='text-2xl font-medium uppercase'>All products</p>
                 <div className='w-16 h-0.5 bg-primary rounded-full'></div>
+            </div>
+
+            <div>
+
             </div>
         </div>
     )
