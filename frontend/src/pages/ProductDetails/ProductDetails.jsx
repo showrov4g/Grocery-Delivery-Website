9f45341e1c04026d3adfd8440f40a83bdb8a0861
showrov4g <showrovg00@gmail.com>
2025-04-22 23:20:52 +0600

    sse more button design

--- frontend/src/pages/ProductDetails/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails/ProductDetails.jsx
@@ -115,7 +115,7 @@ const ProductDetails = () => {
                                 <ProductCart key={index} product={product}/>
                             ))}
                         </div>
-                        <button className='mx-auto cursor-pointer px-12 my-16 py-2.5 border rounded hover:bg-primary/10 transition'>See more</button>
+                        <button onClick={()=>{navigate('/products');scrollTo(0,0)}} className='mx-auto cursor-pointer px-12 my-16 py-2.5 border rounded hover:bg-primary/10 transition'>See more</button>
                     </div>
 
                 )}
