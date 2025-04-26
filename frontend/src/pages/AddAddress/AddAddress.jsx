d6a17a6e704e95b80de968be90090c068daa57cd
showrov4g <showrovg00@gmail.com>
2025-04-26 10:17:39 +0600

    form making

--- frontend/src/pages/AddAddress/AddAddress.jsx
+++ frontend/src/pages/AddAddress/AddAddress.jsx
@@ -2,22 +2,30 @@ import React from 'react'
 import { assets } from '../../assets/assets'
 
 // input field 
-const inputField = ({type, placeholder, name, handleChange, address})=>(
-    <input 
+const InputField = ({type, placeholder, name, handleChange, address}) => (
+  <input 
     className='w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition'
     type={type}
     placeholder={placeholder}
     onChange={handleChange}
     name={name}
-    value={address[name]}
+    value={address?.[name] || ''}
     required
-    />
+  />
 )
 
+
 const AddAddress = () => {
-  const onSubmitHandler = async (e) =>{
+  const onSubmitHandler = async (e) => {
     e.preventDefault();
 
+  }
+  // handle function 
+  const handleChange =()=>{
+
+  }
+  const address =() =>{
+    
   }
 
   return (
@@ -26,9 +34,10 @@ const AddAddress = () => {
       <div className='flex flex-col-reverse md:flex-row justify-between mt-10'>
         <div className='flex-1 max-w-md'>
           <form onSubmit={onSubmitHandler} className='space-y-3 mt-6 text-sm'>
-              <div>
-                <inputField />
-              </div>
+            <div>
+              <InputField handleChange={handleChange} address={address} name="firstName" type="text" placeholder={"First Name"}/>
+              <InputField handleChange={handleChange} address={address} name="lastName" type="text" placeholder={"Last Name"}/>
+            </div>
           </form>
         </div>
         <img className='md:mr-16 mb-16 md:mt-0' src={assets.add_address_iamge} alt="add address" />
