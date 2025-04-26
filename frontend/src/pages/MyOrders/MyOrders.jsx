023803de03bfbd337a50a6000a61899a010b418b
showrov4g <showrovg00@gmail.com>
2025-04-26 11:38:04 +0600

    Ordes page design

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const MyOrders = () => {
-  return (
-    <div>
-        <p>My Orders</p>
-    </div>
-  )
-}
+    return (
+        <div>
+            <div className='flex flex-col items-end mb-8'>
+                <p className='text-2xl font-medium uppercase'>My Orders</p>
+                <div className='w-16 h-0.5 bg-primary rounded-full'></div>
+            </div>
+        </div>
+    )
+} 
 
 export default MyOrders
