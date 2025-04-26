dc6adbbe2eb47355a15d1bd989865b9809f3d68c
showrov4g <showrovg00@gmail.com>
2025-04-26 10:52:15 +0600

    form design complete

--- frontend/src/pages/AddAddress/AddAddress.jsx
+++ frontend/src/pages/AddAddress/AddAddress.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import { assets } from '../../assets/assets'
 
 // input field 
-const InputField = ({type, placeholder, name, handleChange, address}) => (
-  <input 
+const InputField = ({ type, placeholder, name, handleChange, address }) => (
+  <input
     className='w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition'
     type={type}
     placeholder={placeholder}
@@ -28,11 +28,11 @@ const AddAddress = () => {
     phone: '',
   })
 
-  const handleChange =(e)=>{
+  const handleChange = (e) => {
     e.preventDefault();
-    const {name, value} = e.target;
+    const { name, value } = e.target;
 
-    setAddress((pervAdress)=>({
+    setAddress((pervAdress) => ({
       ...AddAddress,
       [name]: value,
 
@@ -53,11 +53,21 @@ const AddAddress = () => {
           <form onSubmit={onSubmitHandler} className='space-y-3 mt-6 text-sm'>
 
             <div className='grid grid-cols-2 gap-4'>
-              <InputField handleChange={handleChange} address={address} name="firstName" type="text" placeholder={"First Name"}/>
-              <InputField handleChange={handleChange} address={address} name="lastName" type="text" placeholder={"Last Name"}/>
+              <InputField handleChange={handleChange} address={address} name="firstName" type="text" placeholder={"First Name"} />
+              <InputField handleChange={handleChange} address={address} name="lastName" type="text" placeholder={"Last Name"} />
             </div>
-            <InputField handleChange={handleChange} address={address} name="email" type="email" placeholder={"Your email"}/>
-            <InputField handleChange={handleChange} address={address} name="street" type="text" placeholder={"Street Name"}/>
+            <InputField handleChange={handleChange} address={address} name="email" type="email" placeholder={"Your email"} />
+            <InputField handleChange={handleChange} address={address} name="street" type="text" placeholder={"Street Name"} />
+            <div className='grid grid-cols-2 gap-4'>
+              <InputField handleChange={handleChange} address={address} name="city" type="text" placeholder={"city"} />
+              <InputField handleChange={handleChange} address={address} name="state" type="text" placeholder={"State"} />
+            </div>
+            <div className='grid grid-cols-2 gap-4'>
+              <InputField handleChange={handleChange} address={address} name="zipcode" type="number" placeholder={"Zip Code"} />
+              <InputField handleChange={handleChange} address={address} name="country" type="text" placeholder={"country"} />
+            </div>
+            <InputField handleChange={handleChange} address={address} name="phone" type="number" placeholder={"Phone Number"}/>
+            <button className='w-full mt-6 bg-primary text-white py-3 hover:bg-primary-dull transition cursor-pointer uppercase'>Save Address</button>
           </form>
         </div>
         <img className='md:mr-16 mb-16 md:mt-0' src={assets.add_address_iamge} alt="add address" />
