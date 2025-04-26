63aeef4e9788768b55023d7fb1ff84e622855e97
showrov4g <showrovg00@gmail.com>
2025-04-26 11:29:20 +0600

    my orders page design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import ProductCategory from './pages/productCategory/ProductCategory'
 import ProductDetails from './pages/ProductDetails/ProductDetails'
 import Cart from './pages/Cart/Cart'
 import AddAddress from './pages/AddAddress/AddAddress'
+import MyOrders from './pages/MyOrders/Myorders'
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -30,6 +31,8 @@ const App = () => {
           <Route path='/products/:category/:id' element={<ProductDetails/>} />
           <Route path='/cart' element={<Cart/>} />
           <Route path='/add-address' element={<AddAddress/>} />
+          <Route path='/my-orders' element={<MyOrders/>} />
+
         </Routes>
       </div>
       {!isSellerPath && <Footer/>}
