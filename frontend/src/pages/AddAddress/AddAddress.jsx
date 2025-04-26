c829b180e66336f2f2e415c64c7772ce83cc13ba
showrov4g <showrovg00@gmail.com>
2025-04-26 09:35:40 +0600

    onsubbmut handler function making

--- frontend/src/pages/AddAddress/AddAddress.jsx
+++ frontend/src/pages/AddAddress/AddAddress.jsx
@@ -7,13 +7,18 @@ const inputField = (()=>(
 ))
 
 const AddAddress = () => {
+  const onSubmitHandler = async (e) =>{
+    e.preventDefault();
+    
+  }
+
   return (
     <div className='mt-16 pb-16'>
       <p className='text-2xl md:text-3xl text-gray-500'>Add shipping <span className='font-semibold text-primary '>Address</span></p>
       <div className='flex flex-col-reverse md:flex-row justify-between mt-10'>
         <div className='flex-1 max-w-md'>
-          <form>
-            
+          <form onSubmit={onSubmitHandler}>
+
           </form>
         </div>
         <img className='md:mr-16 mb-16 md:mt-0' src={assets.add_address_iamge} alt="add address" />
