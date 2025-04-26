a533ea50183ab738dbde8ddace4ecae57e7eb36b
showrov4g <showrovg00@gmail.com>
2025-04-26 10:38:28 +0600

    address form making

--- frontend/src/pages/AddAddress/AddAddress.jsx
+++ frontend/src/pages/AddAddress/AddAddress.jsx
@@ -28,8 +28,15 @@ const AddAddress = () => {
     phone: '',
   })
 
-  const handleChange =()=>{
-    
+  const handleChange =(e)=>{
+    e.preventDefault();
+    const {name, value} = e.target;
+
+    setAddress((pervAdress)=>({
+      ...AddAddress,
+      [name]: value,
+
+    }))
   }
 
   const onSubmitHandler = async (e) => {
@@ -44,10 +51,13 @@ const AddAddress = () => {
       <div className='flex flex-col-reverse md:flex-row justify-between mt-10'>
         <div className='flex-1 max-w-md'>
           <form onSubmit={onSubmitHandler} className='space-y-3 mt-6 text-sm'>
-            <div>
+
+            <div className='grid grid-cols-2 gap-4'>
               <InputField handleChange={handleChange} address={address} name="firstName" type="text" placeholder={"First Name"}/>
               <InputField handleChange={handleChange} address={address} name="lastName" type="text" placeholder={"Last Name"}/>
             </div>
+            <InputField handleChange={handleChange} address={address} name="email" type="email" placeholder={"Your email"}/>
+            <InputField handleChange={handleChange} address={address} name="street" type="text" placeholder={"Street Name"}/>
           </form>
         </div>
         <img className='md:mr-16 mb-16 md:mt-0' src={assets.add_address_iamge} alt="add address" />
