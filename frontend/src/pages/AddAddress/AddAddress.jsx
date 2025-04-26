d83a840a1a11a4e4344b0ec05c0495563e6c88fc
showrov4g <showrovg00@gmail.com>
2025-04-26 09:47:05 +0600

    input field design

--- frontend/src/pages/AddAddress/AddAddress.jsx
+++ frontend/src/pages/AddAddress/AddAddress.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import { assets } from '../../assets/assets'
 
 // input field 
-const inputField = (()=>(
-    <input type="text" />
-))
+const inputField = ({type, placeholder, name, handleChange, address})=>(
+    <input 
+    className='w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition'
+    type={type}
+    placeholder={placeholder}
+    onChange={handleChange}
+    name={name}
+    value={address[name]}
+    required
+    />
+)
 
 const AddAddress = () => {
   const onSubmitHandler = async (e) =>{
     e.preventDefault();
-    
+
   }
 
   return (
@@ -17,8 +25,10 @@ const AddAddress = () => {
       <p className='text-2xl md:text-3xl text-gray-500'>Add shipping <span className='font-semibold text-primary '>Address</span></p>
       <div className='flex flex-col-reverse md:flex-row justify-between mt-10'>
         <div className='flex-1 max-w-md'>
-          <form onSubmit={onSubmitHandler}>
-
+          <form onSubmit={onSubmitHandler} className='space-y-3 mt-6 text-sm'>
+              <div>
+                <inputField />
+              </div>
           </form>
         </div>
         <img className='md:mr-16 mb-16 md:mt-0' src={assets.add_address_iamge} alt="add address" />
