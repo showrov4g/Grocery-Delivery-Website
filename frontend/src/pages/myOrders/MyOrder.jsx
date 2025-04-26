89e49a71ae88d148929cfc03a2ee04710194c477
showrov4g <showrovg00@gmail.com>
2025-04-26 21:01:28 +0600

    page peacing corrections

--- frontend/src/pages/myOrders/MyOrder.jsx
+++ frontend/src/pages/myOrders/MyOrder.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MyOrder = () => {
   return (
-    <div>
+    <div className='mt-16 pb-16'>
       <div className='flex flex-col items-end w-max mb-8'>
         <p className='text-2xl font-medium uppercase'>My orders</p>
         <div className='w-16 h-0.5 bg-primary rounded-full '></div>
