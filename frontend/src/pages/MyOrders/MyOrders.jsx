db3d0c79b7ecdb74b4db900175b16c2be6fd5bd4
showrov4g <showrovg00@gmail.com>
2025-04-26 11:46:37 +0600

    update code

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Myorders = () => {
     return (
         <div>
+            {/* my orders page  */}
             <div className='flex flex-col items-end mb-8'>
                 <p className='text-2xl font-medium uppercase'>My Orders</p>
                 <div className='w-16 h-0.5 bg-primary rounded-full'></div>
