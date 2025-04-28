94412ed2187294faf85a1e6b5dfd476b16062367
showrov4g <showrovg00@gmail.com>
2025-04-28 10:14:26 +0600

    state update

--- frontend/src/components/Seller/SellerLogin/SellerLogin.jsx
+++ frontend/src/components/Seller/SellerLogin/SellerLogin.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAppContext } from '../../../context/AppContext'
 
 const SellerLogin = () => {
+    const {isSeller, setIsSeller, navigate, } = useAppContext();
+    const [email ,setEmail] = useState('');
+    const [passwrod, setPassword] = useState('');
   return (
     <div>
       <h1>Seller login</h1>
