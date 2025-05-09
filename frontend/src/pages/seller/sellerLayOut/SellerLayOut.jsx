9dc8874b37b7eb3bbe7c4c5217673053d31fa4b5
showrov4g <showrovg00@gmail.com>
2025-05-09 18:44:16 +0600

    logout functions add

--- frontend/src/pages/seller/sellerLayOut/SellerLayOut.jsx
+++ frontend/src/pages/seller/sellerLayOut/SellerLayOut.jsx
@@ -1,29 +1,31 @@
 import React from 'react'
 import { useAppContext } from '../../../context/AppContext';
 import { assets } from '../../../assets/assets';
+import {Link} from "react-router-dom"
 
 const SellerLayOut = () => {
     // context and state
     const {isSeller} = useAppContext();
 
 
- 
-  
-
     const sidebarLinks = [
         { name: "Add Product", path: "/seller", icon: assets.add_icon },
         { name: "Product list", path: "/seller/product-list", icon: assets.product_list_icon},
         { name: "Orders", path: "/seller/orders", icon: assets.order_icon},
     ];
+    const logout=()=>{
+        
+    }
+
     return (
         <div>
-            <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white transition-all duration-300">
-                <a href="/">
-                    <img className="h-9" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/dummyLogoColored.svg" alt="dummyLogoColored" />
-                </a>
+            <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white ">
+                <Link to={'/'}>
+                <img src={assets.logo} alt="logo" className='cursor-pointer w-34 md:w-38' />
+                </Link>
                 <div className="flex items-center gap-5 text-gray-500">
                     <p>Hi! Admin</p>
-                    <button className='border rounded-full text-sm px-4 py-1'>Logout</button>
+                    <button onClick={logout} className='border rounded-full text-sm px-4 py-1'>Logout</button>
                 </div>
             </div>
             <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
